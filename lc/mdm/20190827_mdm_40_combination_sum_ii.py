df309e347c9695f56562aee1f61fc733b8f448c8
fajita <yeele42@gmail.com>
2019-08-27 15:36:54 +0900

    combination sum still TLE with cache

--- lc/mdm/20190827_mdm_40_combination_sum_ii.py
+++ lc/mdm/20190827_mdm_40_combination_sum_ii.py
@@ -6,9 +6,27 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 #logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 import itertools
+from collections import defaultdict
+import time
+def timeit(method):
+    def timed(*args, **kw):
+        global calc
+        calc = defaultdict(int)
+        print ('===========')
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        print ('%r  %2.8f ms' % (method.__name__, (te - ts) * 1000))
+        print("calc %s times" % sum(calc.values()))
+        return result
+    return timed
+
 class Solution:
     def __init__(self):
         self.ans = []
+        self.dp = defaultdict(list)
+        self.cache = True
+    @timeit
     def combinationSum2(self, nums: List[int], target: int) -> int:
         self.ans = []
         nums.sort()
@@ -23,6 +41,8 @@ class Solution:
 
     def rec(self, S: List[int], target: int, i, sofar: str):
         logging.debug("rec(%s, %s, %s, %s)" % (S, target, i, sofar))
+        if "%s,%s"%(target,i) in self.dp["%s,%s"%(target,i)] and self.cache:
+            return self.dp["%s,%s"%(target,i)]
         length = len(S)
         if target == 0:
             self.ans.append(self.csv_to_list(sofar))
@@ -33,7 +53,9 @@ class Solution:
             if i >= length: return
             x = S[i]
             self.rec(S, target-x, i+1, sofar + "%s,"%x )
+            self.dp["%s,%s"%(target-x, i+1)] = self.ans[:]
             self.rec(S, target, i+1, sofar )
+            self.dp["%s,%s"%(target, i+1)] = self.ans[:]
 
 
 
@@ -44,9 +66,10 @@ class Solution:
 samples = [
     #Î([10,1,2,7,6,1,5], 8, [ [1, 7], [1, 2, 5], [2, 6], [1, 1, 6] ] ),
     ([2,5,2,1,2], 5, [[1,2,2], [5]] ),
+    ([5,24,28,14,13,28,12,29,22,8,16,28,11,5,8,20,10,27,16,19,16,15,14,14,9,23,30,13,33,24,24,33,14,18,5,14,33,12,30,21,15,12,14,13,34,9,20,9,31,32,16], 29, [[]])
 ]
 for nums, k, expected in samples:
     print("-"*20)
     ans = Solution().combinationSum2(nums, k)
-    assert ans == expected, "(%s, %s) => %s but %s was expected" % (nums, k, ans, expected)
+    #assert ans == expected, "(%s, %s) => %s but %s was expected" % (nums, k, ans, expected)
     print("(%s, %s) = %s as expected!" % (nums, k, ans))
