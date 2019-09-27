e5a07b533c26af30a9b5a8a581f1ab37748d075c
fajita <yeele42@gmail.com>
2019-09-28 00:14:10 +0900

    lc/esy/20190927_esy_141_linked_list_cycle.py

---
+++ lc/esy/20190927_esy_141_linked_list_cycle.py
@@ -0,0 +1,29 @@
+#-*- coding: utf-8 -*-
+from typing import List
+import math
+
+import time
+def timeit(func):
+    def wrapped(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        elapsed = time.time() - start
+        print("elapsed: %s" % elapsed)
+        return ret
+    return wrapped
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        curr = head
+        while curr:
+            if curr.val == "done":
+                return True
+            curr.val = "done"
+            curr = curr.next
+        return False
